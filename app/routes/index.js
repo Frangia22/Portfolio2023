801d620397f52138d02b136dd4f15056068378ad
Frangia22 <giacherofranc22@gmail.com>
2023-04-10 10:34:35 -0300

    Rutas site

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fran Giachero || Desarrollador Web' });
 });
+/* GET projects. */
+router.get('/projects', function(req, res, next) {
+  res.render('pages/projects', { title: 'Proyectos' });
+});
+/* GET about me. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('pages/aboutme', { title: 'Sobre mi' });
+});
+/* GET contact. */
+router.get('/contact', function(req, res, next) {
+  res.render('pages/contact', { title: 'Contacto' });
+});
 
 module.exports = router;
