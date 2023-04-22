1c1d58f285de60ff5237a612ed5deb27c106a98e
Frangia22 <giacherofranc22@gmail.com>
2023-04-21 23:45:55 -0300

    Agregar y mostrar habilidades

--- app/api/index.js
+++ app/api/index.js
@@ -2,3 +2,24 @@
 const { sequelize, Op } = require('sequelize');
 //Importo los modelos
 const db = require('../models');
+
+//Agregar Tecnologia
+const addTech = async (technology, icon) => {
+    const tech = await db.technologies.create({
+        technology,
+        icon
+    });
+    return tech;
+};
+//Traer todas las tecnologias
+const getTech = async () => {
+    const techs = await db.technologies.findAll()
+        .then(result => {
+            return result;
+        });
+    return techs;
+};
+module.exports = {
+    addTech,
+    getTech
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const api = require('../api');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fran Giachero || Desarrollador Web' });
+router.get('/', async(req, res, next) => {
+  const techs = await api.getTech();
+  res.render('index', { title: 'Fran Giachero || Desarrollador Web', techs });
 });
+/* Add technologies - Post - Capturo datos desde form y los envio a bd */
+router.post('/addTech', async (req, res) => {
+  const { technology, icon } = req.body;
+  await api.addTech(technology, icon);
+  const techs = await api.getTech();
+  res.render('index', { title: 'Fran Giachero || Desarrollador Web', techs })
+});
+
 /* GET projects. */
 router.get('/projects', function(req, res, next) {
   res.render('pages/projects', { title: 'Proyectos' });
