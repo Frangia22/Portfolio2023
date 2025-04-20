577bda4e4c65203830c4e5a3118861d408a57168
Frangia22 <giacherofranc22@gmail.com>
2025-04-20 18:22:51 -0300

    feat: Update page metadata and add theme switch functionality

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Home } from './views/Home'
-import { About } from './views/About'
+import { SwitchTheme } from './components/Switch/SwitchTheme'
 
 function App() {
   return (
     <main className='dark:bg-[#131C24] bg-[#F5F7FA] min-h-screen'>
       <Router>
+        <SwitchTheme />
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
