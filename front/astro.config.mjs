bfbc7a8d8f97224b91395bc1e027f83f22bad89f
Frangia22 <giacherofranc22@gmail.com>
2026-03-23 16:40:24 -0300

    feat: Integrate passthroughImageService in Astro config for improved image handling and add sharp package for image processing capabilities

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -6,6 +6,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     plugins: [tailwindcss()]
   },
